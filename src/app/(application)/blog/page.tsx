1d560334843654a892df18d337d32f62288592a5
Toni <tonigashi999@gmail.com>
2024-08-01 21:07:27 -0400

    making blog page dynamic

--- src/app/(application)/blog/page.tsx
+++ src/app/(application)/blog/page.tsx
@@ -7,31 +7,31 @@ import dayjs from "dayjs";
 
 async function Page() {
   const blogs = await getBlogs();
+  const featuredBlog = blogs.find(blog => blog.isFeatured)
 
   return (
     <main className="flex flex-col">
       <div className="flex justify-center">
         <div className="flex max-w-[1220px] flex-col items-center justify-center">
-          <div className="flex gap-20">
+          {featuredBlog && <div className="flex items-center gap-20">
             <div className="flex flex-col gap-5">
               <CustomChip label="Featured" width={80} />
               <div className="flex flex-col gap-3">
                 <h2 className="text-[50px] font-medium leading-[67px]">
-                  A complete travel guide to Siesta & Longboat Key
+                  {featuredBlog?.title}
                 </h2>
                 <p className="mb-2 leading-[30px] text-gray-600">
-                  Repeatedly named the best beach in the country, it is baffling
-                  how discovered Siesta Key beach still is. The white sand
-                  against the crystal clear turquoise waters of the Gulf of
-                  Mexico is jaw-dropping enough but add in the lack of...
+                  {featuredBlog?.description.substr(0, 350) + '\u2026'}
                 </p>
-                <CustomButton label="Read more" width={160} />
+                <Link href={`/blog/${featuredBlog.id}`}>
+                  <CustomButton label="Read more" width={160} />
+                </Link>
               </div>
             </div>
             <div className="flex h-[370px] w-[530px] shrink-0">
               <Image
                 alt="Blog image"
-                src="/blog_page_image.png"
+                src={featuredBlog?.thumbnailImageUrl}
                 className="rounded-2xl"
                 quality={90}
                 width={0}
@@ -44,13 +44,13 @@ async function Page() {
                 }}
               />
             </div>
-          </div>
+          </div>}
           <div className="mt-10 grid grid-cols-4 gap-8 py-10">
-            {blogs.map((card) => {
+            {blogs.filter(blog => blog.id !== featuredBlog?.id).map((card) => {
               return (
                 <Link key={card.id} href={`/blog/${card.id}`} className="h-82">
                   <MainCard
-                    imageUrl="/blog_photo.jpeg"
+                    imageUrl={card.thumbnailImageUrl}
                     name={card.title}
                     subtitle={`${dayjs(card.createdOn).format('MMMM d, YYYY')}  •  ${card.readTime} min`}
                     key={card.id}
