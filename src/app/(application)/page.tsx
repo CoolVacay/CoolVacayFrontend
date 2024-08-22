403f7c10740d5dd927c37ec40c4b014439eeebd1
Toni <tonigashi999@gmail.com>
2024-08-22 18:10:57 -0400

    reverting priority of main image

--- src/app/(application)/page.tsx
+++ src/app/(application)/page.tsx
@@ -33,6 +33,7 @@ export default async function HomePage() {
             src="/landing_background.png"
             quality={20}
             fill
+            priority={true}
             style={{
               position: "absolute",
               objectFit: "cover",
