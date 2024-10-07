7eed2df052b4e2586cccb04f66cd8a2555a1dd38
Toni <tonigashi999@gmail.com>
2024-10-07 15:17:49 -0400

    typo

--- src/app/(application)/(search-param-context-pages)/book/[source]/[id]/payment/BookingCompleted.tsx
+++ src/app/(application)/(search-param-context-pages)/book/[source]/[id]/payment/BookingCompleted.tsx
@@ -11,7 +11,7 @@ export default function BookingCompleted() {
       />
       <div className="flex flex-col items-center gap-3 text-[#676D73]">
         <h1 className="text-2xl font-bold text-black">Thank you!</h1>
-        <h2 className="text-[16px]">Your booking details has been sent to your email.</h2>
+        <h2 className="text-[16px]">Your booking details have been sent to your email.</h2>
         <h3 className="text-[16px]">
           You can download the booking as PDF below, or check it on the
           dashboard
