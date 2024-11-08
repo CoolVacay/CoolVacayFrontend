62a389d79fcf68fd4682735b0514381110e9b425
Toni <tonigashi999@gmail.com>
2024-11-08 12:57:20 -0500

    fixing build issues

--- src/app/(application)/actions.ts
+++ src/app/(application)/actions.ts
@@ -32,7 +32,6 @@ export const getSiteConfigurations = async () =>
   fetcher<IReadOnlySiteConfigurationProperties>(
     `SiteConfiguration`,
     "Failed to fetch configurations",
-    true,
   );
 
 export const getCountries = async () =>
