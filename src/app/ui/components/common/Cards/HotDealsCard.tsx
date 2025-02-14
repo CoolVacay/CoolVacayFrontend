e1f79148dc56a58ed1e417e0eff8c9a6f6366c38
Toni <tonigashi999@gmail.com>
2025-02-14 08:24:06 -0500

    adding the option to have optional pricing

--- src/app/ui/components/common/Cards/HotDealsCard.tsx
+++ src/app/ui/components/common/Cards/HotDealsCard.tsx
@@ -31,32 +31,67 @@ function HotDealsCard({ listing }: { listing: IHotDealData }) {
             <LocationPin color="#29ABE2" />
             <span className="ml-1">{listing?.address}</span>
           </div>
-          <div className="flex items-center gap-2">
-            <p className="text-sm font-semibold italic text-primary sm:text-base">
-              Hot Deal Price:
-            </p>
-            <div className="flex items-center">
-              <div className="relative flex items-center">
-                {/* Discount Circle */}
-                <div className="z-10 flex h-8 w-8 items-center justify-center rounded-full border-2 border-white bg-primary text-xs font-bold text-white md:h-10 md:w-10 md:text-sm">
-                  {listing?.discountPercentage}%
+          {/* If price exists in the hot deal, we show the crossed price and new price, otherwise we just show the discount percentage */}
+          {!listing.price ? (
+            <div className="flex items-center gap-2">
+              <p className="text-sm font-semibold italic text-primary sm:text-base">
+                Hot Deal Price:
+              </p>
+              <div className="flex items-center">
+                <div className="relative flex items-center">
+                  <div className="z-10 flex h-8 w-8 items-center justify-center rounded-full border-2 border-white bg-primary text-xs font-bold text-white md:h-10 md:w-10 md:text-sm">
+                    {listing?.discountPercentage}%
+                  </div>
+                  <div className="absolute left-6 bg-gray-200 py-1.5 pl-3 pr-4 text-xs font-bold text-gray-800 md:left-7 md:py-2 md:pl-4 md:pr-6 md:text-sm">
+                    OFF
+                  </div>
+                  <div
+                    className="absolute left-[70px] z-20 h-0 w-0 md:left-[88px]"
+                    style={{
+                      borderTop: "16px solid transparent",
+                      borderBottom: "16px solid transparent",
+                      borderRight: "8px solid #FFF",
+                    }}
+                  ></div>
                 </div>
-                {/* Discount Label */}
-                <div className="absolute left-6 bg-gray-200 py-1.5 pl-3 pr-4 text-xs font-bold text-gray-800 md:left-7 md:py-2 md:pl-4 md:pr-6 md:text-sm">
-                  OFF
-                </div>
-                {/* Decorative Triangle */}
-                <div
-                  className="absolute left-[70px] z-20 h-0 w-0 md:left-[88px]"
-                  style={{
-                    borderTop: "16px solid transparent",
-                    borderBottom: "16px solid transparent",
-                    borderRight: "8px solid #FFF",
-                  }}
-                ></div>
               </div>
             </div>
-          </div>
+          ) : (
+            <div>
+              <p className="font-semibold italic text-primary">
+                Hot Deal Price:
+              </p>
+              <div className="flex items-center gap-2">
+                <span className="line-through">
+                  $
+                  {Math.round(
+                    (listing.price * 100) / (100 - listing.discountPercentage),
+                  )}
+                </span>
+                <span className="text-2xl font-bold">
+                  ${listing.price}/night
+                </span>
+                <div className="flex items-center">
+                  <div className="relative flex items-center">
+                    <div className="z-10 flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-primary text-sm font-bold text-white">
+                      {listing?.discountPercentage}%
+                    </div>
+                    <div className="absolute left-7 bg-gray-200 py-2 pl-4 pr-6  text-sm font-bold text-gray-800">
+                      OFF
+                    </div>
+                    <div
+                      className="absolute left-[88px] z-20 h-0 w-0"
+                      style={{
+                        borderTop: "20px solid transparent",
+                        borderBottom: "20px solid transparent",
+                        borderRight: "10px solid #FFF",
+                      }}
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
           <p className="mt-2 line-clamp-5 text-sm md:max-w-[400px]">
             {listing?.listingDescription}
           </p>
